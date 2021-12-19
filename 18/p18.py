e84101899061e1e468450caed9c1a8a232b7fcfe
Tomev <rybotycki.tomasz+github@gmail.com>
2021-12-19 17:13:46 +0100

    Working on solution to day 18 problems.

--- 18/p18.py
+++ 18/p18.py
@@ -4,37 +4,85 @@ __author__ = "Tomasz Rybotycki"
     This script contains solutions to day 18 problems.
 """
 
+from copy import deepcopy
 
 class SnailNumber:
-    def __init__(self, left, right, nesting_level = 0):
+    def __init__(self, left, right, nesting_level = 0, parent = None):
         self.nesting_level = nesting_level
+        self.parent = parent
 
         if type(left) is list:
-            left = SnailNumber(left[0], left[1], nesting_level + 1)
+            left = SnailNumber(left[0], left[1], nesting_level, self)
+
+        if type(left) is SnailNumber:
+            left = deepcopy(left)
+            left.increase_nesting_level()
 
         self.left = left
 
         if type(right) is list:
-            right = SnailNumber(right[0], right[1], nesting_level + 1)
+            right = SnailNumber(right[0], right[1], nesting_level, self)
+
+        if type(right) is SnailNumber:
+            right = deepcopy(right)
+            right.increase_nesting_level()
 
         self.right = right
 
+
     def __add__(self, other):
-        return SnailNumber(self, other)
+        a = deepcopy(self)
+        b = deepcopy(other)
+        return SnailNumber(a, b)
+
+    def increase_nesting_level(self):
+        self.nesting_level += 1
+
+        if type(self.left) == SnailNumber:
+            self.left.increase_nesting_level()
+
+        if type(self.right) == SnailNumber:
+            self.right.increase_nesting_level()
 
     def __str__(self):
         return f"[{str(self.left)}, {str(self.right)}]"
 
+    def explode(self) -> bool:
+
+        exploded = False
+
+        if type(self.left) is SnailNumber:
+            if self.left.nesting_level == 4:
+                exploded = True
+                self.right += self.left.right
+                self.left = 0
+            else:
+                exploded = self.left.explode()
+
+        if type(self.right) is SnailNumber:
+            if self.right.nesting_level == 4:
+                exploded = True
+                self.left += self.right.left
+                self.right = 0
+            else:
+                exploded = self.right.explode() or exploded
+
+        return exploded
+
+    def get_root(self):
+        root = self
+        while root.parent is not None:
+            root = root.parent
+        return root
+
 def p1() -> None:
-    a = SnailNumber(1, 2)
-    b = SnailNumber(3, [4, 5])
-    c = a + b
-    print(a)
-    print(b)
-    print(c)
-    d = c + c
-    print(d)
-    print(d.right.right.right.nesting_level)
+    a = SnailNumber([6,[5,[4,[3,2]]]],1)
+    a_root = a.get_root()
+    print(a_root)
+    print(a.left.get_root())
+    a_r_r_root = a.left.right.right.right.get_root()
+    print(a_r_r_root)
+
 
 
 
